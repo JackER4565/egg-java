fb0dd942214342066883efc413c9b0c8f63c5b54
Fabrizio Rondinella <fabrondinella@gmail.com>
2023-05-22 18:50:24 -0300

    initial

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // System.in.read();
+        int ran = (int) (Math.random() * 100);
+        System.out.println(ran);
+    }
+}
+
+    
